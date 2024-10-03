78731677cf06d80013589cc0338e99653d1f3d85
Radoya aftab <raimaaftab73@gmail.com>
2024-10-03 16:22:43 +0600

    first commit

---
+++ set.py
@@ -0,0 +1,15 @@
+mySet = {1,2,3,3,3,4,5,5}
+print(mySet)
+
+mySet.add("Radoya")
+mySet.add("zabir")
+
+print(mySet)
+
+mySet.pop()
+mySet.pop()
+print(mySet)
+
+l1 = [1,2,3,2]
+setlist = set(l1)
+print(setlist)
